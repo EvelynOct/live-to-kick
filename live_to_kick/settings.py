c78f769b3d51601749f351b1699e6f71ba422a88
EvelynOct <evelyne.aritonang72@gmail.com>
2025-09-09 21:07:57 +0700

    Assignment 2

--- live_to_kick/settings.py
+++ live_to_kick/settings.py
@@ -31,7 +31,8 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "evelyne-octaviana-livetokick.pbp.cs.ui.ac.id"]
+
 
 
 # Application definition
