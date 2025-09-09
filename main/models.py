3f41dc3a6563ad45f4a2c76a9e91da7dbf8f4180
EvelynOct <evelyne.aritonang72@gmail.com>
2025-09-10 06:12:24 +0700

    Completed assignment 2

--- main/models.py
+++ main/models.py
@@ -19,14 +19,15 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update') # product category
     thumbnail = models.URLField(blank=True, null=True) # product image
     is_featured = models.BooleanField(default=False) # featured product
+    views = models.IntegerField(default=0) # number of views
 
     def __str__(self):
         return self.title
 
     @property
-    def is_news_hot(self):
-        return self.news_views > 20
+    def popular_item(self):
+        return self.views > 50
 
     def increment_views(self):
-        self.news_views += 1
+        self.views += 1
         self.save()
--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        "app_name": "Live to Kick",
         'npm' : '2406365282',
         'name': 'Evelyne Octaviana Benedicta Aritonang',
         'class': 'KKI'
