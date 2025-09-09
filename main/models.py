07f800aa5068a1136a12e38a47f420b1ee09a9d5
EvelynOct <evelyne.aritonang72@gmail.com>
2025-09-09 22:18:32 +0700

    Halfway completed assignment 2

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@
 import uuid
 from django.db import models
 
-class News(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = [
         ('transfer', 'Transfer'),
         ('update', 'Update'),
@@ -15,13 +15,13 @@ class News(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
-    thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=255) ##
+    price = models.IntegerField() ##
+    description = models.TextField()  # item description
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update') ##
+    thumbnail = models.URLField(blank=True, null=True) ##
     created_at = models.DateTimeField(auto_now_add=True)
-    is_featured = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False) ##
 
     def __str__(self):
         return self.title
