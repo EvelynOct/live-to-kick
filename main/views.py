c38217e674050ba6cfee50130052006875aeb031
EvelynOct <evelyne.aritonang72@gmail.com>
2025-09-17 08:16:53 +0700

    cleanup

--- main/views.py
+++ main/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import ProductForm
+from main.models import Product
 
-# Create your views here.
 def show_main(request):
+    product_list = Product.objects.all()
+
     context = {
-        "app_name": "Live to Kick",
         'npm' : '2406365282',
-        'name': 'Evelyne Octaviana Benedicta Aritonang',
-        'class': 'KKI'
+        'name': 'Evelyne O.B Aritonang',
+        'class': 'KKI',
+        'product_list': product_list
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.increment_views()
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "product_detail.html", context)
